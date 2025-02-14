07731afa37b89c27cd9da94599f8537d099f8404
Riya <soniriya2409@gmail.com>
2025-02-14 20:23:59 +0530

    [Riya] Solution of Cheecked Exception

--- Day04/src/main/java/com/checkedexception/IOExceptionExample.java
+++ Day04/src/main/java/com/checkedexception/IOExceptionExample.java
@@ -1,4 +1,29 @@
 package com.checkedexception;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+//Create the class name IOExceptionExample
 public class IOExceptionExample {
+
+    //Main Method
+    public static void checkedException(){
+        //Define the path
+        String fileName = "D:/riya/data.txt";
+        //Use try-catch block to handle exception
+        try{
+            //BufferReader Object
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch(FileNotFoundException e){
+            System.out.println("File not found"+e.getMessage());
+        } catch(IOException e) {
+            System.out.println("An error occurred while reading the file"+e.getMessage());
+        }
+    }
 }
--- Day04/src/main/java/com/checkedexception/Main.java
+++ Day04/src/main/java/com/checkedexception/Main.java
@@ -1,4 +1,7 @@
 package com.checkedexception;
 
 public class Main {
+    public static void main(String args[]){
+        IOExceptionExample.checkedException();
+    }
 }
