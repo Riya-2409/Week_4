6a56a0402a3a1fe1306d14e9911733eaf7a3cb42
Riya <soniriya2409@gmail.com>
2025-02-14 20:27:00 +0530

    [Riya] Solution of Multiple Catch Block

--- Day04/src/main/java/com/multiplecatchblock/Main.java
+++ Day04/src/main/java/com/multiplecatchblock/Main.java
@@ -1,4 +1,7 @@
 package com.multiplecatchblock;
 
 public class Main {
+    public static void main(String args[]){
+        MultipleCatchBlockExample.multipleCatchBlock();
+    }
 }
--- Day04/src/main/java/com/multiplecatchblock/MultipleCatchBlockExample.java
+++ Day04/src/main/java/com/multiplecatchblock/MultipleCatchBlockExample.java
@@ -1,4 +1,41 @@
 package com.multiplecatchblock;
 
+import java.util.Scanner;
+
+//Create the class name MultipleCatchBlockExample
 public class MultipleCatchBlockExample {
+
+       //Main Method
+        public static void multipleCatchBlock() {
+            //Take input from the user
+            Scanner sc = new Scanner(System.in);
+            //Use try-catch block to handle exception
+            try {
+                System.out.print("Enter the number of element in array:");
+                int size = sc.nextInt();
+                int arr[] = new int[size];
+
+                System.out.println("Enter " + size + " Element");
+                for (int i = 0; i < size; i++) {
+                    arr[i] = sc.nextInt();
+                }
+
+                System.out.print("Enter the index:");
+                int index = sc.nextInt();
+
+                if (arr == null) {
+                    throw new NullPointerException();
+                }
+                if (index < 0 || index >= arr.length) {
+                    throw new ArrayIndexOutOfBoundsException();
+                }
+                System.out.println("Value at index " + index + ":" + arr[index]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Invalid index!"+e.getMessage());
+            } catch (NullPointerException e) {
+                System.out.println("Array is not initialized!"+e.getMessage());
+            }
+        }
 }
+
+
