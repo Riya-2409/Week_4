b51f59124736b1728693aa5178bcc0140ce5491e
Riya <soniriya2409@gmail.com>
2025-02-14 20:25:53 +0530

    [Riya] Solution of exceptionPropagation in method

--- Day04/src/main/java/com/exceptionpropagationinmethod/ExceptionPropagationExample.java
+++ Day04/src/main/java/com/exceptionpropagationinmethod/ExceptionPropagationExample.java
@@ -1,4 +1,14 @@
 package com.exceptionpropagationinmethod;
 
 public class ExceptionPropagationExample {
+
+    // Method that causes an ArithmeticException
+    public static void method1() {
+        int result = 10 / 0; // This will cause ArithmeticException
+    }
+
+    // Method that calls method1()
+    public static void method2() {
+        method1();
+    }
 }
--- Day04/src/main/java/com/exceptionpropagationinmethod/Main.java
+++ Day04/src/main/java/com/exceptionpropagationinmethod/Main.java
@@ -1,4 +1,12 @@
 package com.exceptionpropagationinmethod;
 
 public class Main {
+
+    public static void main(String[] args) {
+        try {
+            ExceptionPropagationExample.method2();
+        } catch (ArithmeticException e) {
+            System.out.println("Handled exception in main");
+        }
+    }
 }
