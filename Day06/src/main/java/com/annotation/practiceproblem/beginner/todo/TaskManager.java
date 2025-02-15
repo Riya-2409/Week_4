549ef4f863c06b26209342bb925402a88a56e4c9
Riya <soniriya2409@gmail.com>
2025-02-16 02:43:49 +0530

    [Riya] Add. Todo Annotation

--- Day06/src/main/java/com/annotation/practiceproblem/beginner/todo/TaskManager.java
+++ Day06/src/main/java/com/annotation/practiceproblem/beginner/todo/TaskManager.java
@@ -1,4 +1,19 @@
 package com.annotation.practiceproblem.beginner.todo;
 
 public class TaskManager {
+
+    @Todo(task = "Implement authentication", assignedTo = "Alice", priority = "HIGH")
+    public void implementAuth() {
+        System.out.println("Implementing authentication...");
+    }
+
+    @Todo(task = "Optimize database queries", assignedTo = "Bob")
+    public void optimizeDatabase() {
+        System.out.println("Optimizing database queries...");
+    }
+
+    @Todo(task = "Refactor legacy code", assignedTo = "Charlie", priority = "LOW")
+    public void refactorCode() {
+        System.out.println("Refactoring legacy code...");
+    }
 }
--- Day06/src/main/java/com/annotation/practiceproblem/beginner/todo/Todo.java
+++ Day06/src/main/java/com/annotation/practiceproblem/beginner/todo/Todo.java
@@ -1,4 +1,15 @@
 package com.annotation.practiceproblem.beginner.todo;
 
-public class todo {
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+// Define @Todo annotation
+@Retention(RetentionPolicy.RUNTIME) // Available at runtime for reflection
+@Target(ElementType.METHOD) // Can be used on methods
+@interface Todo {
+    String task(); // Task description
+    String assignedTo(); // Developer responsible
+    String priority() default "MEDIUM"; // Default priority is MEDIUM
 }
