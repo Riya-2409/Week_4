3ca0388cdd41ef457bd5c8642e33ab51b04f043f
Riya <soniriya2409@gmail.com>
2025-02-16 01:46:09 +0530

    [Riya] Add. Testing File Handling Methods

--- Day05/src/test/java/com/junit/basics/testingexceptionhandling/ExceptionHandlerTest.java
+++ Day05/src/test/java/com/junit/basics/testingexceptionhandling/ExceptionHandlerTest.java
@@ -1,4 +1,24 @@
 package com.junit.basics.testingexceptionhandling;
 
+import com.junit.basic.testingexceptionhandling.ExceptionHandler;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class ExceptionHandlerTest {
+
+    @Test
+    void testDivision() {
+        assertEquals(5, ExceptionHandler.divide(10, 2));
+        assertEquals(-3, ExceptionHandler.divide(-9, 3));
+    }
+
+    @Test
+    void testDivisionByZero() {
+        Exception exception = assertThrows(ArithmeticException.class, () -> {
+            ExceptionHandler.divide(10, 0);
+        });
+
+        assertEquals("Division by zero is not allowed", exception.getMessage());
+    }
 }
