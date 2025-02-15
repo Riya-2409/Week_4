f8c8102fe218e1731f916dd00bebb73dc87400c9
Riya <soniriya2409@gmail.com>
2025-02-16 02:30:34 +0530

    [Riya] Add. Use Deprecated

--- Day06/src/main/java/com/annotation/exercise/usedeprecated/LegacyAPI.java
+++ Day06/src/main/java/com/annotation/exercise/usedeprecated/LegacyAPI.java
@@ -1,4 +1,13 @@
 package com.annotation.exercise.usedeprecated;
 
 public class LegacyAPI {
+
+    @Deprecated
+    public void oldFeature() {
+        System.out.println("Old feature: This method is deprecated.");
+    }
+
+    public void newFeature() {
+        System.out.println("New feature: Use this method instead.");
+    }
 }
--- Day06/src/main/java/com/annotation/exercise/usedeprecated/Main.java
+++ Day06/src/main/java/com/annotation/exercise/usedeprecated/Main.java
@@ -1,4 +1,11 @@
 package com.annotation.exercise.usedeprecated;
 
 public class Main {
+
+    public static void main(String[] args) {
+        LegacyAPI api = new LegacyAPI();
+
+        api.oldFeature(); // Should show a warning during compilation
+        api.newFeature();
+    }
 }
