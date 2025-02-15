c01d5650e5ddfab5715088c64380a43586778a7f
Riya <soniriya2409@gmail.com>
2025-02-16 01:44:37 +0530

    [Riya] Add. Tetsing Parametrized Tests

--- Day05/src/test/java/com/junit/basics/testingparameterizedtests/NumberUtilsTest.java
+++ Day05/src/test/java/com/junit/basics/testingparameterizedtests/NumberUtilsTest.java
@@ -1,4 +1,20 @@
 package com.junit.basics.testingparameterizedtests;
 
-public class NumberUtilsTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.junit.basic.testingparameterizedtests.NumberUtils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class NumberUtilsTest {
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 8, 10})
+    void testIsEvenTrue(int number) {
+        assertTrue(NumberUtils.isEven(number), number + " should be even.");
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 7, 9})
+    void testIsEvenFalse(int number) {
+        assertFalse(NumberUtils.isEven(number), number + " should be odd.");
+    }
 }
