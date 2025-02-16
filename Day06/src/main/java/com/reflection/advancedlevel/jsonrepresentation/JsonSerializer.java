8f84c1c5970c64e4d616bd01a6f2c93d87a33ae1
Riya <soniriya2409@gmail.com>
2025-02-16 14:55:31 +0530

    [Riya] Add. JSON Representation

--- Day06/src/main/java/com/reflection/advancedlevel/jsonrepresentation/JsonSerializer.java
+++ Day06/src/main/java/com/reflection/advancedlevel/jsonrepresentation/JsonSerializer.java
@@ -1,4 +1,41 @@
 package com.reflection.advancedlevel.jsonrepresentation;
 
+import java.lang.reflect.Field;
+
 public class JsonSerializer {
+
+    public static String toJson(Object obj) {
+        if (obj == null) {
+            return "null";
+        }
+
+        StringBuilder json = new StringBuilder("{");
+        Class<?> clazz = obj.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+
+        for (int i = 0; i < fields.length; i++) {
+            fields[i].setAccessible(true); // Allow access to private fields
+            try {
+                json.append("\"").append(fields[i].getName()).append("\": ");
+
+                Object value = fields[i].get(obj);
+
+                if (value instanceof String) {
+                    json.append("\"").append(value).append("\"");
+                } else {
+                    json.append(value);
+                }
+            } catch (IllegalAccessException e) {
+                json.append("\"error\"");
+            }
+
+            if (i < fields.length - 1) {
+                json.append(", ");
+            }
+        }
+
+        json.append("}");
+        return json.toString();
+    }
 }
+
--- Day06/src/main/java/com/reflection/advancedlevel/jsonrepresentation/Main.java
+++ Day06/src/main/java/com/reflection/advancedlevel/jsonrepresentation/Main.java
@@ -1,4 +1,10 @@
 package com.reflection.advancedlevel.jsonrepresentation;
 
 public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Alice", 25, false);
+        String json = JsonSerializer.toJson(person);
+        System.out.println(json);
+    }
 }
+
