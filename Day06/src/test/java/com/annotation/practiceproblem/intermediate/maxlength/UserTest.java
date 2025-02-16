bf9a738ee8f42863fc1133152dcd96fa12b10e5f
Riya <soniriya2409@gmail.com>
2025-02-16 11:45:46 +0530

    [Riya] Add. Create a @MaxLength Annotation for Field Validation

--- Day06/src/test/java/com/annotation/practiceproblem/intermediate/maxlength/UserTest.java
+++ Day06/src/test/java/com/annotation/practiceproblem/intermediate/maxlength/UserTest.java
@@ -1,4 +1,18 @@
 package com.annotation.practiceproblem.intermediate.maxlength;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UserTest {
+    @Test
+    void testValidUsername() {
+        User user = new User("ValidUser");
+        assertEquals("ValidUser", user.getUsername());
+    }
+
+    @Test
+    void testUsernameExceedsMaxLength() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new User("TooLongUsername"));
+        assertEquals("username exceeds maximum length of 10 characters", exception.getMessage());
+    }
 }
