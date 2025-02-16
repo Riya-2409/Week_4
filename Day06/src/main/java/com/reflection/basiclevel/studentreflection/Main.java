51f3e80eb609f6d4f189b230860e1cfa2951ddc3
Riya <soniriya2409@gmail.com>
2025-02-16 13:52:32 +0530

    [Riya] Add. Student Reflection

--- Day06/src/main/java/com/reflection/basiclevel/studentreflection/Main.java
+++ Day06/src/main/java/com/reflection/basiclevel/studentreflection/Main.java
@@ -1,4 +1,26 @@
 package com.reflection.basiclevel.studentreflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 public class Main {
+
+    public static void main(String[] args) {
+        try {
+
+            Class<?> studentClass = Class.forName("Student");
+
+
+            Constructor<?> constructor = studentClass.getDeclaredConstructor();
+
+
+            Object studentInstance = constructor.newInstance();
+
+            Method displayMethod = studentClass.getMethod("display");
+            displayMethod.invoke(studentInstance);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
