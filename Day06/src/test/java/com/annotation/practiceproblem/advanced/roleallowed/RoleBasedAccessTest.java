1e21a377766aa758ff7d6183a1d5f681b817dc02
Riya <soniriya2409@gmail.com>
2025-02-16 12:17:45 +0530

    [Riya] Add.  Implement a Role-Based Access Control with @RoleAllowed

--- Day06/src/test/java/com/annotation/practiceproblem/advanced/roleallowed/RoleBasedAccessTest.java
+++ Day06/src/test/java/com/annotation/practiceproblem/advanced/roleallowed/RoleBasedAccessTest.java
@@ -1,4 +1,20 @@
 package com.annotation.practiceproblem.advanced.roleallowed;
 
+import org.junit.jupiter.api.Test;
+
 public class RoleBasedAccessTest {
+
+    @Test
+    void testAdminAccess() {
+        AccessControl adminControl = new AccessControl("ADMIN");
+        SecureService service = new SecureService();
+        adminControl.executeMethod(service, "adminTask");
+    }
+
+    @Test
+    void testUserAccessDenied() {
+        AccessControl userControl = new AccessControl("USER");
+        SecureService service = new SecureService();
+        userControl.executeMethod(service, "adminTask"); // Should print "Access Denied!"
+    }
 }
