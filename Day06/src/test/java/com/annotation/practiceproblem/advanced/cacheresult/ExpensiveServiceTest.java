1d76796d06635ab3532c1bf51f289a7687f00d8d
Riya <soniriya2409@gmail.com>
2025-02-16 12:19:44 +0530

    [Riya] Add. Implement a Custom Caching System with @CacheResult

--- Day06/src/test/java/com/annotation/practiceproblem/advanced/cacheresult/ExpensiveServiceTest.java
+++ Day06/src/test/java/com/annotation/practiceproblem/advanced/cacheresult/ExpensiveServiceTest.java
@@ -1,4 +1,17 @@
 package com.annotation.practiceproblem.advanced.cacheresult;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ExpensiveServiceTest {
+
+    @Test
+    void testComputeSquareCaching() {
+        ExpensiveService service = new ExpensiveService();
+        int firstCall = service.computeSquare(5);
+        int secondCall = service.computeSquare(5);
+        assertEquals(25, firstCall);
+        assertEquals(25, secondCall);
+    }
 }
