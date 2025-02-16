7bd430ab2008b26029cf365aa65a65efb1b3f672
Riya <soniriya2409@gmail.com>
2025-02-16 13:51:53 +0530

    [Riya] Add. Person Reflection

--- Day06/src/test/java/com/reflection/basiclevel/personreflection/PersonReflectionTest.java
+++ Day06/src/test/java/com/reflection/basiclevel/personreflection/PersonReflectionTest.java
@@ -1,4 +1,24 @@
 package com.reflection.basiclevel.personreflection;
 
-public class PersonReflectionTest {
+import java.lang.reflect.Field;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonReflectionTest {
+    @Test
+    void testPersonAgeModification() {
+        try {
+            Person person = new Person(25);
+            Field ageField = Person.class.getDeclaredField("age");
+            ageField.setAccessible(true);
+
+            int initialAge = (int) ageField.get(person);
+            assertEquals(25, initialAge, "Initial age should be 25");
+
+            ageField.set(person, 30);
+            assertEquals(30, person.getAge(), "Updated age should be 30");
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            fail("Reflection operation failed: " + e.getMessage());
+        }
+    }
 }
