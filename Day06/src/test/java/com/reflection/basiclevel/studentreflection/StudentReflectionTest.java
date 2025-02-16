9085c820d9674bef0bf30cf7f96ccc39a37e1979
Riya <soniriya2409@gmail.com>
2025-02-16 13:52:41 +0530

    [Riya] Add. Student Reflection

--- Day06/src/test/java/com/reflection/basiclevel/studentreflection/StudentReflectionTest.java
+++ Day06/src/test/java/com/reflection/basiclevel/studentreflection/StudentReflectionTest.java
@@ -1,4 +1,22 @@
 package com.reflection.basiclevel.studentreflection;
 
-public class StudentReflectionTest {
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentReflectionTest {
+    @Test
+    void testStudentReflection() {
+        try {
+            Class<?> studentClass = Student.class;
+            Constructor<?> constructor = studentClass.getDeclaredConstructor();
+            Object studentInstance = constructor.newInstance();
+
+            Method displayMethod = studentClass.getMethod("display");
+            displayMethod.invoke(studentInstance);
+        } catch (Exception e) {
+            fail("Reflection operation failed: " + e.getMessage());
+        }
+    }
 }
