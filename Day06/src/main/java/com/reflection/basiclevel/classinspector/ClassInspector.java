d502ba04d524615f63cc8ede656b8799bb220bc6
Riya <soniriya2409@gmail.com>
2025-02-16 13:50:52 +0530

    [Riya] Add. Class Inspector

--- Day06/src/main/java/com/reflection/basiclevel/classinspector/ClassInspector.java
+++ Day06/src/main/java/com/reflection/basiclevel/classinspector/ClassInspector.java
@@ -1,4 +1,39 @@
 package com.reflection.basiclevel.classinspector;
 
+import java.lang.reflect.Constructor;
+import java.util.*;
+import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+
 public class ClassInspector {
+
+    public static void inspectClass(String className) {
+        try {
+
+            Class<?> clazz = Class.forName(className);
+
+
+            System.out.println("\nClass Name: " + clazz.getName());
+
+
+            System.out.println("\nConstructors:");
+            Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+            for (Constructor<?> constructor : constructors) {
+                System.out.println(" " + constructor);
+            }
+            System.out.println("\nFields:");
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                System.out.println(" " + field);
+            }
+            System.out.println("\nMethods:");
+            Method[] methods = clazz.getDeclaredMethods();
+            for (Method method : methods) {
+                System.out.println(" " + method);
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found. Please enter a valid class name.");
+        }
+    }
 }
--- Day06/src/main/java/com/reflection/basiclevel/classinspector/Main.java
+++ Day06/src/main/java/com/reflection/basiclevel/classinspector/Main.java
@@ -1,4 +1,13 @@
 package com.reflection.basiclevel.classinspector;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the fully qualified class name: ");
+        String className = scanner.nextLine();
+        ClassInspector.inspectClass(className);
+        scanner.close();
+    }
 }
